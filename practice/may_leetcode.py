350957da620f78d79f534f984e940012b33ee1bf
iniubongisemin <eugeneinie@gmail.com>
2025-05-27 09:26:18 +0100

    Tuesday leetcode challenge crushed!!!

--- practice/may_leetcode.py
+++ practice/may_leetcode.py
@@ -42,14 +42,39 @@ def list_str(listt):
     all_list_string = []
     strng = ""
     integer = ""
+    str_count = 0
+    int_count = 0
 
     for i in listt:
         if isinstance(i, str):
             strng += i
+            str_count += 1
+
         if isinstance(i, int):
             j = str(i)
             integer += j
+            int_count += 1
     all_list_string.append(integer)
     all_list_string.append(strng)
-    print(all_list_string)
-list_str(lit)
+    print(all_list_string, f"INTEGER_COUNT: {int_count}, STRING_COUNT: {str_count}")
+# list_str(lit)
+
+"""
+28. Even Numbers Until 237
+Write a Python program to print all even numbers from a given list of numbers in the same order and stop printing any after 237 in the sequence.
+Sample numbers list :
+"""
+sample_numbers = [    
+    386, 462, 47, 418, 907, 344, 236, 375, 823, 566, 597, 978, 328, 615, 953, 345, 
+    399, 162, 758, 219, 918, 237, 412, 566, 826, 248, 866, 950, 626, 949, 687, 217, 
+    815, 67, 104, 58, 512, 24, 892, 894, 767, 553, 81, 379, 843, 831, 445, 742, 717, 
+    958,743, 527
+    ]
+
+def even_num_till(number:list):
+    for i in number:
+        if i != 237:
+            print(i)
+        else:
+            continue
+even_num_till(sample_numbers)
