03764ef2087bd8026a3bcb9b847b62dec3391c0e
iniubongisemin <eugeninie@gmail.com>
2026-01-05 12:09:58 +0100

    Revision of OOP - Inheritance

--- oop/oop.py
+++ oop/oop.py
@@ -83,17 +83,17 @@ class Wallet:
         if 0 < amount <= self.__balance:
             self.__balance -= amount
 
-    def get_balace(self):
+    def get_balance(self):
         return self.__balance
     
 account = Wallet(500)
-print(f"Here's your account balance ₦{account.get_balace()}")
+print(f"Here's your account balance ₦{account.get_balance()}")
 
 account.deposit(100000)
-print(f"Here's your account balance ₦{account.get_balace()}")
+print(f"Here's your account balance ₦{account.get_balance()}")
 
 account.withdraw(25000)
-print(f"Here's your account balance ₦{account.get_balace()}")
+print(f"Here's your account balance ₦{account.get_balance()}")
 
 # You can also define a private __validate method to check if every deposit or withdrawal amount is a positive number:
 class Account:
@@ -122,3 +122,155 @@ my_account.deposit(10000000)
 print(f"Here's your account balance ₦{my_account.get_balance()}")
 my_account.withdraw(5000000)
         
+"GETTERS & SETTERS"
+"Getters and setters are methods that let you control how the attributes of a class are accessed and modified."
+
+"These actions are done through what's known as properties. They are what connect getters and setters, and allow access to data."
+
+"Properties act like attributes but behave like methods under the hood." 
+"PPS: You can access properties with dot notation instead of parentheses or round brackets -- Their main purpose is readability!"
+
+class Circle:
+    def __init__(self, radius):
+        self._radius = radius
+
+    @property
+    def radius(self): # A getter to get the radius
+        return self._radius
+    
+    @property
+    def area(self): # A getter to calculate area
+        return 3.14 * (self._radius ** 2)
+    
+my_circle = Circle(3)
+print("RADIUS>>>>", my_circle.radius)
+print("AREA>>>>", my_circle.area)
+
+"USING PROPERTIES TO SET ATTRIBUTES"
+"SYNTAX: @<property_name>.setter"
+
+class Circle:
+    def __init__(self, radius):
+        self._radius = radius
+
+    @property
+    def radius(self): # A getter to get the radius
+        return self._radius
+    
+    @radius.setter
+    def radius(self, value): # A setter to set the radius
+        if value <= 0:
+            raise ValueError("Radius MUST be positive!")
+        self._radius = value
+
+gold_circle = Circle(3)
+print("INITIAL RADIUS>>> ", gold_circle.radius) 
+
+gold_circle.radius = 8
+print("MODIFIED RADIUS>>> ", gold_circle.radius) 
+
+"NB: Once you define getters and setters, Python automatically calls them under the hood whenever you use normal attribute syntax:"
+"E.g"
+print("INITIAL RADIUS>>> ", gold_circle.radius) # This will call the getter
+gold_circle.radius = 10 # This will call the setter
+
+print("NEW RADIUS>>> ", gold_circle.radius) 
+
+"NB: Note that inside the setter, you cannot use same name of the property when assigning a new value."
+
+"DELETER"
+"A deleter runs custom logic when you use the del statement on a property."
+
+"SYNTAX: @<property_name>.deleter:"
+
+class Circle:
+    def __init__(self, radius):
+        self._radius = radius
+
+    # GETTER
+    @property
+    def radius(self):
+        return self._radius
+    
+    # SETTER
+    @radius.setter
+    def radius(self, value):
+        if value <= 0:
+            raise ValueError("Radius MUST be positive!")
+        self._radius = value
+
+    # DELETER
+    @radius.deleter
+    def radius(self):
+        print("Deleting radius....")
+        del self._radius
+        print("Radius deleted!")
+
+# How to Delete it
+# Create the Instance
+my_circle = Circle(33)
+print("Initial radius: ", my_circle.radius)
+
+# Delete the radius
+del my_circle.radius
+
+# Confirm Deletion
+try:
+    print(my_circle.radius)
+except AttributeError as e:
+    print("Error", str(e))
+
+
+
+"#2. INHERITANCE"
+"With inheritance, a subclass (or child class) can use the attributes and methods of a base class (or parent class). This allows you to reuse code, create clear class hierarchies, and customize behavior without rewriting everything."
+
+# SYNTAX
+class Parent:
+    # Parent atttibutes & Methods
+    pass
+
+class Child(Parent):
+    # Child inherits, extends and/or overrides where necessary
+    pass
+
+"NB: For the Child class to inherit from the Parent class, you have to pass the Parent to the Child." 
+# - SINGLE INHERITANCE i.e The child class inherits from exactly one parent class
+
+"E.g"
+class Animal:
+    def __init__(self, name):
+        self.name = name
+
+    def sound(self):
+        return f"{self.name} makes a sound"
+    
+
+class Dog(Animal):
+    bark = "woof! woof!! woof!!!"
+
+jack = Dog("Jack")
+# print(jack.sound()) 
+# print(jack.bark)
+
+"OVERRIDING THE SOUND METHOD"
+    
+class Dog(Animal):
+    bark = "Woof! Woof!! Woof!!!"
+
+    def sound(self):
+        return f"{self.name} barks {self.bark}"
+    
+scott = Dog("Scott")
+# print(scott.sound())
+
+"NB: You can keep the return value of sound() and add the bark class variable later, you can extend sound() by using the super() function"
+class Dog(Animal):
+    bark = "Woof! Woof!! Woof!!!"
+
+    def sound(self):
+        dunder = super().sound()
+        return f"{dunder}, then {self.name} barks {self.bark}"
+
+tramp = Dog("Tramp")
+print(tramp.sound())
