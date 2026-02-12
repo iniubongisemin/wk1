a72081a11994ad8aa0cb4202e6743adf83caeb0c
iniubongisemin <eugeneinie@gmail.com>
2026-02-12 11:16:54 +0100

    Built freeCodeCamps's Report Card Printer

---
+++ freeCodeCamp/report_card_printer.py
@@ -0,0 +1,13 @@
+name = "Ini-ubong"
+print(name, type(name))
+
+is_founder = True
+print(is_founder, type(is_founder))
+
+age = "2*"
+print(age, type(age))
+
+score = 100
+print(isinstance(score, float))
+
+print(score, type(score))
