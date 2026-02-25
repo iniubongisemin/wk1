8e765b4e2ad633cd44533529e7bc24dec4768a48
iniubongisemin <eugeneinie@gmail.com>
2026-02-25 17:35:11 +0100

    Built FCC's Bill Splitter Exercise

--- freeCodeCamp/booleans_and_conditionals.py
+++ freeCodeCamp/booleans_and_conditionals.py
@@ -183,3 +183,29 @@ elif distance_mi > 6 and (has_car or has_ride_share_app):
 else:
     print(False)
 
+"To calculate a percentage in Python, you can multiply the total by the decimal equivalent of the percentage"
+
+"BILL SPLITTER"
+running_total = 0
+
+num_of_friends = 4
+
+appetizers = 37.89
+main_courses = 57.34
+desserts = 39.39
+drinks = 64.21
+
+running_total = appetizers + main_courses + desserts + drinks
+print('Total bill so far:', running_total)
+
+tip = running_total * 0.25
+print('Tip amount:', tip)
+
+running_total += tip
+print('Total with tip:', running_total)
+
+final_bill = running_total / num_of_friends
+print('Bill per person:', final_bill)
+
+each_pays = round(final_bill, 2)
+print("Each person pays:", each_pays)
